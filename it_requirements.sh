b70f326a7b4950dc09e5a9b8deee7962cb2faa4e
Ganesh M <47482214+mganesh1308@users.noreply.github.com>
2019-07-15 18:57:32 +0530

    added new changes

--- it_requirements.sh
+++ it_requirements.sh
@@ -10,4 +10,4 @@ echo "installing Nightly build of numpy pandas"
 
 PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
 
-pip install --pre --no-deps --upgrade --timeout=60 -f $PRE_WHEELS numpy pandas
+pip install --pre --no-deps --upgrade --no-cache-dir --timeout=60 -f $PRE_WHEELS numpy pandas
