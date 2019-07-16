f7028dc6c211808dc74455d520597e62f3103e59
Ganesh M <47482214+mganesh1308@users.noreply.github.com>
2019-07-16 11:37:32 +0530

    added new changes

--- it_requirements.sh
+++ it_requirements.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -xeo pipefail
-
 echo "uninstalling numpy and pandas"
 
 pip uninstall -y numpy pandas
@@ -10,4 +8,4 @@ echo "installing Nightly build of numpy pandas"
 
 PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
 
-pip install --pre --no-deps --upgrade --no-cache-dir --timeout=60 -f $PRE_WHEELS numpy pandas
+pip install --pre --no-deps --upgrade --timeout=180 --no-cache-dir -f $PRE_WHEELS numpy pandas
