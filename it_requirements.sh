7d819be5a8dd26471c56a5dfa47c455c31637de9
Ganesh M <47482214+mganesh1308@users.noreply.github.com>
2019-07-16 15:08:01 +0530

    added new changes

--- it_requirements.sh
+++ it_requirements.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 
-echo "uninstalling numpy and pandas"
-
-pip uninstall -y numpy pandas
-
-echo "installing Nightly build of numpy pandas"
+echo "Upgrading to  Nightly build of numpy pandas"
 
 PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
 
 pip install --pre --no-deps --upgrade --timeout=180 --no-cache-dir -f $PRE_WHEELS numpy pandas
 
-echo "uninstalling numpy and pandas"
-
-pip uninstall -y pyarrow
-
-echo "installing Nightly build of pyarrow"
+echo "Upgrading to Nightly build of pyarrow"
 
 pip install --pre --no-deps --upgrade --timeout=180 --no-cache-dir -f "https://github.com/ursa-labs/crossbow/releases/download/latest" pyarrow
