9ded638a26e0a123645e99fafdcaebdc2019fdb5
Ganesh M <47482214+mganesh1308@users.noreply.github.com>
2019-07-18 16:32:17 +0530

    numpy_pandas_pyarrow nightly

--- ci/set_pyarrow_integration.sh
+++ ci/set_pyarrow_integration.sh
@@ -4,3 +4,4 @@ echo "Upgrading to Nightly build of pyarrow"
 
 pip install --pre --no-deps --upgrade --timeout=180 --no-cache-dir -f "https://github.com/ursa-labs/crossbow/releases/download/latest" pyarrow
 
+pip freeze
