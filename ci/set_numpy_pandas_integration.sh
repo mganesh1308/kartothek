f21631ca556d1c7c301bb55ca5bc6cd1d6bba032
Ganesh M <47482214+mganesh1308@users.noreply.github.com>
2019-07-22 14:01:58 +0200

    changed numpan nightly files

--- ci/set_numpy_pandas_integration.sh
+++ ci/set_numpy_pandas_integration.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -xeo pipefail
-
 echo "Upgrading to  Nightly build of numpy pandas"
 
 PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
