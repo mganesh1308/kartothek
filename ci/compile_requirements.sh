7a3780688c9b536b9870e4a293f946acd1f96b66
Usha Nemani <usha.nemani@jda.com>
2019-08-05 17:31:28 +0200

    pyarrow numpy changes-1

--- ci/compile_requirements.sh
+++ ci/compile_requirements.sh
@@ -1,17 +1,59 @@
 #!/bin/bash
 set -xeo pipefail
+KARTOTHEK_ARROW_VERSION=0.12.1
+if [ -z ${KARTOTHEK_NP_PD_NIGHTLY} ]; then
+  KARTOTHEK_NP_PD_NIGHTLY=0
+fi
 
-if [ ! -z ${KARTOTHEK_ARROW_VERSION} ];
+if [ "${KARTOTHEK_NP_PD_NIGHTLY}" == 1 ];
 then
-    echo pyarrow==$KARTOTHEK_ARROW_VERSION > kartothek_env_reqs.txt
-    trap 'rm -f kartothek_env_reqs.txt' EXIT
+  echo " KARTOTHEK_NP_PD_NIGHTLY Value--->  $KARTOTHEK_NP_PD_NIGHTLY"
+   PIP_COMPILE_OPTIONS=(
+    "--pre"
+    "-f https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
+    )
+
+    PIP_COMPILE_ARGS=""
+    for opt in "${PIP_COMPILE_OPTIONS[@]}";
+    do
+        PIP_COMPILE_ARGS=$PIP_COMPILE_ARGS" "$opt
+    done
+    trap 'rm -f test-requirements-pinned.txt' EXIT
+    pip-compile\
+        $PIP_COMPILE_ARGS \
+        -o requirements-pinned.txt \
+        requirements.txt
+
+elif [ ${KARTOTHEK_ARROW_VERSION} == "NIGHTLY" ] ;
+then
+    echo " KARTOTHEK_ARROW_VERSION Value--->  $KARTOTHEK_ARROW_VERSION"
+    pyarrow_url=$(python ci/get_pyarrow_nightly.py)
+    echo "pyarrow URl --> $pyarrow_url"
+    KARTOTHEK_ARROW_VERSION=$(python -c "print('$pyarrow_url'.split('/')[-1].split('-')[1])")
+    echo pyarrow=="$KARTOTHEK_ARROW_VERSION" > kartothek_env_reqs.txt
+    trap 'rm -f kartothek_env_reqs.txt requirements-pinned.txt' EXIT
+    pip-compile\
+        --pre \
+        -f "$pyarrow_url" \
+        -o requirements-pinned.txt \
+        kartothek_env_reqs.txt \
+        requirements.txt\
+
+elif [ ! "${KARTOTHEK_ARROW_VERSION}" = "NIGHTLY" ];
+then
+    echo " KARTOTHEK_ARROW_VERSION Value--->  $KARTOTHEK_ARROW_VERSION"
+    echo pyarrow=="$KARTOTHEK_ARROW_VERSION" > kartothek_env_reqs.txt
+    trap 'rm -f kartothek_env_reqs.txt requirements-pinned.txt' EXIT
     pip-compile \
         --upgrade \
         --no-index \
         -o requirements-pinned.txt \
         kartothek_env_reqs.txt \
         requirements.txt
+
 else
+    echo " KARTOTHEK_ARROW_VERSION Value--->  $KARTOTHEK_ARROW_VERSION"
+    trap 'rm -f requirements-pinned.txt' EXIT
     pip-compile \
         --upgrade \
         --no-index \
